a423c1def7e21e75577ca94b06cd021b803b8093
Adrian Costin <adriancostin6@gmail.com>
2021-09-22 18:01:19 +0300

    Modify script to store current wallpaper

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,10 @@
-#fetch json format of bing daily wallpaper, parse it using jq and add it to the bing url to get the photo, then save it in .desktop.jpg
-curl -s http://bing.com$(curl -s http://www.bing.com/HPImageArchive.aspx\?format\=js\&idx\=0\&n\=1 | jq -r '.images[0].url') > ~/.desktop.jpg
+prevwall=$HOME/.config/prev-wallpaper.jpg
+wall=$HOME/.config/wallpaper.jpg
 
-#set wallpaper and generate colorscheme based on it. remove photo afterwards
-feh --bg-fill ~/.desktop.jpg
-rm ~/.desktop.jpg
+feh --bg-fill $prevwall
+
+wget -q -O $wall https://source.unsplash.com/1920x1080/?nature,animals
+
+cp $wall $prevwall
+
+feh --bg-fill $wall
